51c538b0ca2630e630a30d71f2fea0000bbc7f4c
Robert Klacso <robert.klacso@apter.tech>
2023-08-02 09:47:26 +0200

    Initial Commit

---
+++ SnapshotArticle/SnapshotArticleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotArticleApp.swift
+//  SnapshotArticle
+//
+//  Created by Robert Klacso on 2023. 08. 02..
+//
+
+import SwiftUI
+
+@main
+struct SnapshotArticleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
